f516c96b325895a8539cafe5bd3eff98e31ae00d
Thomas E. Dickey <dickey@invisible-island.net>
1990-05-11 11:21:04 -0400

    snapshot of project "ded", label t19900511

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
-# $Id: run_test.sh,v 7.0 1989/12/11 09:52:18 ste_cm Rel $
+# $Id: run_test.sh,v 9.0 1990/05/11 11:21:04 ste_cm Rel $
 # Perform regression tests for unix directory editor.  If we find any problems,
 # show it in the log.
 #
 # $Log: run_test.sh,v $
-# Revision 7.0  1989/12/11 09:52:18  ste_cm
-# BASELINE Mon Apr 30 09:54:01 1990 -- (CPROTO)
+# Revision 9.0  1990/05/11 11:21:04  ste_cm
+# BASELINE Mon Jun 10 10:09:56 1991 -- apollo sr10.3
 #
+# Revision 8.0  90/05/11  11:21:04  ste_cm
+# BASELINE Mon Aug 13 15:06:41 1990 -- LINCNT, ADA_TRANS
+# 
+# Revision 7.1  90/05/11  11:21:04  dickey
+# set umask to make test repeatable
+# 
+# Revision 7.0  89/12/11  09:52:18  ste_cm
+# BASELINE Mon Apr 30 09:54:01 1990 -- (CPROTO)
+# 
 # Revision 6.0  89/12/11  09:52:18  ste_cm
 # BASELINE Thu Mar 29 07:37:55 1990 -- maintenance release (SYNTHESIS)
 # 
@@ -19,6 +28,7 @@
 L=run_tests.out
 date >>$L
 #
+umask 022
 for i in *.cmd
 do
 	F=`basename $i .cmd`
